8c849f831f6604a23b26a568eacb6b8bd519d5a7
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-11-24 17:14:12 -0500

    better view point

--- demo/_attachments/script.js
+++ demo/_attachments/script.js
@@ -1,4 +1,4 @@
-var m = L.map('map').setView([42.3508, -71.0483], 14), 
+var m = L.map('map').setView([42.3336, -71.0966], 13),
 	attributionText = 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', 
 	mq=L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpeg', {attribution : attributionText, subdomains : '1234'}).addTo(m);
 var dbPath = document.location.protocol+"//"+document.location.host+"/"+document.location.pathname.split("/")[1];
